d90969ec3563e97a396e81349c065df9b8106ba6
SamLo <sam@randomidea.com>
2017-07-30 12:19:09 +0800

    add new project

---
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+	FString ObjName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position Report Obj Name = %s"),*ObjName);
+	// ...
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
